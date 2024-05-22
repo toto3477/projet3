c6b7e010902a2c54c4850f10e49406e82142c6ee
costamagno <tommycostamagno@gmail.com>
2024-05-22 17:01:56 +0200

    finalisation

--- FrontEnd/scryptconnexion.js
+++ FrontEnd/scryptconnexion.js
@@ -1,68 +1,42 @@
 
-
-function verifierAuthentification (email, password,) {
-
-    const loginForm = document.querySelector('.formInscription');
-    
-            if (email === response.email.value && password === response.password.value) {
-                alert('Connexion réussie !');
-                
-            } else {
-                const messageIdentificationIncorrect = document.createElement("p");
-                messageIdentificationIncorrect.textContent = "Identifiants incorrects. Veuillez réessayer.";
-                loginForm.appendChild(messageIdentificationIncorrect);
-            }
-    }
-    
-    document.addEventListener('DOMContentLoaded', function() {
-        const loginForm = document.querySelector('.formInscription');
+document.addEventListener('DOMContentLoaded', function() {
+const loginForm = document.querySelector('.formInscription');
         
-        loginForm.addEventListener('submit', function(event) {
-            event.preventDefault(); // Empêcher la soumission du formulaire par défaut
+    loginForm.addEventListener('submit', function(event) {
+        event.preventDefault(); 
             
-            // Récupérer les valeurs des champs email et password
-            const email = document.getElementById('email').value;
-            const password = document.getElementById('password').value;
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
     
-            try {
+        try {
                 // Envoi de la requête POST au serveur pour vérifier les informations de connexion
-                fetch("http://localhost:5678/api/users/login", {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({ email, password })
-                })
-                .then((response) => {
+            fetch("http://localhost:5678/api/users/login", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ email, password })
+            })
+            .then((response) => {
+                if (!response.ok) {
+                    const messageIdentificationIncorrect = document.createElement("p");
+                    messageIdentificationIncorrect.textContent = "Identifiants incorrects. Veuillez réessayer.";
+                    loginForm.appendChild(messageIdentificationIncorrect);
+                    throw new Error('Identifiants incorrects. Veuillez réessayer.');
+                } else {
+                    return response.json()
+                }
+            })
+            .then ((data) => {
+                localStorage.setItem("token", data.token)
+                localStorage.setItem("userId", data.userId)
+                window.location.replace("index.html")  
+            })
+        } catch (error) {
+            alert(error.message);
+        }
     
-                    // Vérification de la réponse du serveur
-                    if (!response.ok) {
-                        alert("mot de passe incorrect")
-                        throw new Error('Identifiants incorrects. Veuillez réessayer.');
-                    } else {
-                        return response.json()
-    
-                    }
-                   
-    
-                })
-                .then ((data) => {
-                    localStorage.setItem("token", data.token)
-                    localStorage.setItem("userId", data.userId)
-                    window.location.replace("index.html")
-    
-                })
-                .then (() =>{
-                    verifierAuthentification(email, password)
-
-                })
-    
-            } catch (error) {
-                // Affichage du message d'erreur
-                alert(error.message);
-            }
-    
-        });
     });
+});
 
 
